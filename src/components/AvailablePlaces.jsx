1fcfb9a5be271fe9dd88a8e9d3eb9b9a44557b48
ZMP <zane.prunella@gmail.com>
2024-02-24 19:26:17 -0800

    HTTP error handle

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,26 +1,50 @@
 import { useState, useEffect } from "react";
+import Error from "./Error";
 
 import Places from "./Places.jsx";
 
 const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [availablePlaces, setAvailablePlaces] = useState([]);
+  // Common to have these three below.
+  const [isFetching, setIsFetching] = useState(false); // Loading, fetching state
+  const [availablePlaces, setAvailablePlaces] = useState([]); // Data State
+  const [error, setError] = useState(); // Error state, show potential errors
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+
+      try {
+        const response = await fetch("http://localhost:3000/placess");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch");
+        } //200, 300 good, 400, 500 not good
         setAvailablePlaces(resData.places);
-      });
+      } catch (error) {
+        setError({
+          message: error.message || "Could not fetch places, please try agian.",
+        });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occured!" message={error.message} />;
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data.."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
